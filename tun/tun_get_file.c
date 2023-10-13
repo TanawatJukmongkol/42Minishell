8e4b456fbdf3512a422f32de66d27d950bfa3fcf
misterjune <sounddevcloud145@gmail.com>
2023-10-14 02:20:40 +0700

    Accidentally put infile var in outfile's function (FIXED)

--- tun/tun_get_file.c
+++ tun/tun_get_file.c
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 02:08:33 by tponutha          #+#    #+#             */
-/*   Updated: 2023/10/13 14:12:39 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/10/14 02:10:43 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	tun_get_infile(t_token_stream subset, t_exec *exe)
 {
-	size_t	i;
-	size_t	j;
-	int		err;
+	int	i;
+	int	j;
+	int	err;
 
 	i = 1;
 	j = 0;
@@ -24,7 +24,7 @@ int	tun_get_infile(t_token_stream subset, t_exec *exe)
 	if (exe->in_len == 0)
 		return (err);
 	exe->infile[0] = STDIN_FILENO;
-	while (subset.begin != NULL && err == 1 && errno != ENOMEM)
+	while (subset.begin != NULL && err == 1 && errno != ENOMEM && i < exe->in_len)
 	{
 		if (subset.begin->type == __redirr_in)
 		{
@@ -47,27 +47,27 @@ int	tun_get_infile(t_token_stream subset, t_exec *exe)
 
 int	tun_get_outfile(t_token_stream subset, t_exec *exe)
 {
-	size_t	i;
-	int		err;
+	int	i;
+	int	err;
 
 	i = 1;
 	err = 1;
 	if (exe->out_len == 0)
 		return (err);
 	exe->outfile[0] = STDOUT_FILENO;
-	while (subset.begin != NULL && err == 1 && errno != ENOMEM)
+	while (subset.begin != NULL && err == 1 && errno != ENOMEM && i < exe->out_len)
 	{
 		if (subset.begin->type == __redirr_append)
 		{
 			exe->outfile[i] = tun_open(subset.begin->value, \
 								O_WRONLY | O_APPEND | O_CREAT, 0644);
-			err = exe->infile[i] != -1;
+			err = exe->outfile[i] != -1;
 		}
 		else if (subset.begin->type == __redirr_trunc)
 		{
 			exe->outfile[i] = tun_open(subset.begin->value, \
 								O_WRONLY | O_TRUNC | O_CREAT, 0644);
-			err = exe->infile[i] != -1;
+			err = exe->outfile[i] != -1;
 		}
 		i += subset.begin->type == __redirr_append \
 			|| subset.begin->type == __redirr_trunc;
@@ -81,7 +81,7 @@ void	tun_get_argv(t_token_stream subset, t_exec *exe)
 	int		i;
 
 	i = 0;
-	while (subset.begin != NULL)
+	while (subset.begin != NULL && exe->argv[i] != NULL)
 	{		
 		if (subset.begin->type == __cmd || subset.begin->type == __argv)
 		{
