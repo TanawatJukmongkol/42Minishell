fdc2d0808d942b02b99c850cb543868edd4d1980
Tanawat Jukmongkol <tjukmong@z3t4c4.42bangkok.com>
2023-10-18 05:41:27 +0700

    bruh

---
+++ minishell.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjukmong <tjukmong@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 18:00:31 by tjukmong          #+#    #+#             */
+/*   Updated: 2023/10/18 00:06:32 by tjukmong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	g_is_child_running;
+
+void	sig_quit(int signum)
+{
+	if (signum == SIGQUIT)
+	{
+		rl_redisplay();
+	}
+}
+
+void	sig_int(int signum)
+{
+	if (signum == SIGINT && g_is_child_running == 0)
+	{
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+int	repl(t_token_stream *lex, t_main *info)
+{
+	t_token_stream	*box;
+	size_t			pipe_n;
+	char			*prompt;
+	char			*line;
+
+	prompt = ft_dynamic_prompt(info);
+	line = ft_readline(prompt);
+	free(prompt);
+	if (!prompt || !line)
+		return (1);
+	if (line[0] != 0)
+	{
+		g_is_child_running = 1;
+		lexer(lex, line, info);
+		parser(&info->_token, lex);
+		if (info->_token.begin == NULL)
+			exit(ft_clear_main(info, 0));
+		box = tun_split_token(info, &pipe_n);
+		tun_parent_process(info, box, pipe_n);
+		tun_free_token_box(box, pipe_n);
+		g_is_child_running = 0;
+	}
+	else
+		free(line);
+	return (0);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_token_stream	lex;
+	t_main			info;
+
+	(void)av;
+	if (ac != 1)
+		return (0);
+	g_is_child_running = 0;
+	if (ft_signal() == 0)
+		return (0);
+	if (ft_init_main(&info, envp) == -1)
+		return (ENOMEM);
+	lex.begin = NULL;
+	lex.last = NULL;
+	while (!repl(&lex, &info))
+		;
+	printf("exit\n");
+	return (ft_clear_main(&info, 0));
+}
---
+++ minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjukmong <tjukmong@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 01:06:46 by tjukmong          #+#    #+#             */
+/*   Updated: 2023/10/18 04:36:25 by tjukmong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "libminishell.h"
+# include "pun.h"
+# include "tun.h"
+
+// MS_DEBUG = 1 -> debug mode
+// MS_DEBUG = 0 -> product mode
+
+# ifndef MS_DEBUG
+#  define MS_DEBUG 1
+# endif
+
+#endif
